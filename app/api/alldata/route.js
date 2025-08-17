aa5cd51924fe695f3ecebb2b3d0db3517817c79d
Nithya7138 <nithyaprasadk01@gmail.com>
2025-08-17 19:54:07 +0530

    content  change

--- app/api/alldata/route.js
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import data from "@/app/data/academies.json";
-
-export default function handler(req, res) {
-  res.status(200).json(data);
-}
--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function HomePage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">Academy</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">Academys</h1>
 
       <div className="flex flex-col md:flex-row justify-center gap-4 mb-8">
       
