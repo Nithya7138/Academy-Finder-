5d68063bc66fad4fba9b8863da9d894b312d2b67
Nithya7138 <nithyaprasadk01@gmail.com>
2025-08-17 19:25:21 +0530

    js to ts

--- app/api/alldata/route.js
+++ app/api/alldata/route.js
@@ -1,6 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import data from "@/app/data/academies.json";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req, res) {
   res.status(200).json(data);
 }
