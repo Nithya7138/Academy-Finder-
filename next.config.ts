9c0d164bd2ea80f21e2b5a828fe86dfd5a6863ad
Nithya7138 <nithyaprasadk01@gmail.com>
2025-08-20 12:07:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    API_URL: 'http://localhost:3004'
+    API_URL: 'app/api/academic'
   }
 };
 
